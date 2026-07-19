6b418a37203d04492d756f2e29ecac77d40a9c36
Nartey <bigj11514@gmail.com>
2026-07-19 22:34:55 +0000

    landing page

---
+++ .js
@@ -0,0 +1,14 @@
+let buttonClick = "Which social do you want to open?";
+
+switch (buttonClick){
+    case 1:
+        console.log("do you want to open Whatsapp?")
+    brake;
+    case 2:
+        console.log("do you want to open Instagram?")
+    breake;
+    case 3:
+        console.log("do you wan to open Tick Tock?")
+    brake;
+
+}
